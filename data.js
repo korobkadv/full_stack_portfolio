e30ec0a5763f7cd0a9189aafa8a3bafcd0cd09d1
korobkadv <korobka_dv@ukr.net>
2024-09-10 15:30:17 +0300

    add full stack portfolio

---
+++ data.js
@@ -0,0 +1,121 @@
+const category = [
+  {
+    name: "HTML / CSS",
+    urlNAme: "html",
+  },
+  {
+    name: "Java Script",
+    urlNAme: "js",
+  },
+  {
+    name: "React JS",
+    urlNAme: "react",
+  },
+  {
+    name: "Back End",
+    urlNAme: "back_end",
+  },
+  {
+    name: "CMS",
+    urlNAme: "cms",
+  },
+];
+
+const demos = [
+  {
+    category: "html",
+    name: "WebStudio",
+    url: "https://korobkadv.github.io/WebStudio-HTML-CSS/",
+    img: "webstudio.png",
+  },
+  {
+    category: "react",
+    name: "Moto Store",
+    url: "http://demo.idvk.info/react/motoshop/",
+    img: "motoshop.png",
+  },
+  {
+    category: "react",
+    name: "Movie Search",
+    url: "https://korobkadv.github.io/goit-react-hw-05-movies/",
+    img: "movies.png",
+  },
+  {
+    category: "react",
+    name: "Photo Gallery",
+    url: "http://demo.idvk.info/react/photos/",
+    img: "photos.png",
+  },
+  {
+    category: "react",
+    name: "Image Search",
+    url: "https://korobkadv.github.io/goit-react-hw-04-images/",
+    img: "images_search.png",
+  },
+  {
+    category: "react",
+    name: "Phonebook",
+    url: "https://korobkadv.github.io/goit-react-hw-08-phonebook/contacts/",
+    img: "phonebook.png",
+  },
+  {
+    category: "react",
+    name: "Money exchange",
+    url: "http://demo.idvk.info/react/moneychange/",
+    img: "moneychange.png",
+  },
+  {
+    category: "react",
+    name: "Sending invitations",
+    url: "http://demo.idvk.info/react/users/",
+    img: "users.png",
+  },
+  {
+    category: "react",
+    name: "Statistics feedback",
+    url: "https://korobkadv.github.io/goit-react-hw-04-feedback/",
+    img: "feedback.png",
+  },
+  {
+    category: "js",
+    name: "Alarm Clock",
+    url: "http://demo.idvk.info/js/alarmclock/",
+    img: "alarm_clock.png",
+  },
+  {
+    category: "back_end",
+    name: "Node.JS Back End",
+    url: "https://github.com/korobkadv/NodeJs-Backend",
+    img: "backend.png",
+  },
+  {
+    category: "js",
+    name: "Game moto jump",
+    url: "http://demo.idvk.info/js/game_moto_jump",
+    img: "game_moto_jump.png",
+  },
+  {
+    category: "cms",
+    name: "WorPress WooCommerce store",
+    url: "http://demo.idvk.info/wp/motostore/",
+    img: "WP_WooCommerce_Moto_Store.png",
+  },
+  {
+    category: "js",
+    name: "Neo Wake Up message",
+    url: "http://demo.idvk.info/js/neo/",
+    img: "neo.png",
+  },
+  {
+    category: "js",
+    name: "Car mockup",
+    url: "http://demo.idvk.info/js/car_mockup/",
+    img: "car_mockup.png",
+  },
+  {
+    category: "back_end",
+    name: "Telegram BOT",
+    url: "https://t.me/dumbieBOT",
+    img: "telegram_bot.png",
+  },
+];
---
+++ script.js
@@ -0,0 +1,130 @@
+// Ініціалізація Three.js сцени
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById("three-canvas"),
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Створення відео текстури
+const video = document.createElement("video");
+video.src = "img/hero-background.mp4"; // Шлях до відео файлу
+video.loop = true;
+video.muted = true;
+video.play();
+
+const videoTexture = new THREE.VideoTexture(video);
+scene.background = videoTexture; // Встановлюємо відео як фон для сцени
+
+// Завантаження текстур для кожної грані куба
+const loader = new THREE.TextureLoader();
+const materials = [
+  new THREE.MeshBasicMaterial({ map: loader.load("img/javascript.jpg") }), // грань з "JavaScript"
+  new THREE.MeshBasicMaterial({ map: loader.load("img/react.jpg") }), // грань з "React"
+  new THREE.MeshBasicMaterial({ map: loader.load("img/node.png") }), // грань з "Node.js"
+  new THREE.MeshBasicMaterial({ map: loader.load("img/html.jpg") }), // грань з "HTML5"
+  new THREE.MeshBasicMaterial({ map: loader.load("img/css.jpg") }), // грань з "CSS3"
+  new THREE.MeshBasicMaterial({ map: loader.load("img/git.png") }), // грань з "Git"
+];
+
+// Функція для створення куба з врахуванням 60% від висоти вікна
+function createCube(cubeWidth = 3, cubeHeight = 3) {
+  const geometry = new THREE.BoxGeometry(cubeWidth, cubeHeight, cubeWidth); // Пропорційний куб
+  const cube = new THREE.Mesh(geometry, materials);
+  return cube;
+}
+
+// Додаємо куб до сцени
+let cube = createCube();
+scene.add(cube);
+
+// Позиціонуємо камеру
+camera.position.z = 5;
+
+// Анімація куба
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+animate();
+
+// Функція для оновлення розміру куба при зміні розміру вікна
+function onWindowResize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+
+  // Видаляємо старий куб і додаємо новий з оновленим розміром
+  scene.remove(cube);
+  if (window.innerWidth <= 500) {
+    cubeHeight = 2;
+    cubeWidth = 2;
+  } else {
+    cubeHeight = 3;
+    cubeWidth = 3;
+  }
+  cube = createCube(cubeHeight, cubeWidth);
+  scene.add(cube);
+}
+
+// Додаємо подію для оновлення розміру куба при зміні розміру вікна
+window.addEventListener("resize", onWindowResize);
+
+// Інтерактивність на рух миші
+window.addEventListener("mousemove", function (event) {
+  const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+  const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  cube.rotation.x += mouseY * 0.05;
+  cube.rotation.y += mouseX * 0.05;
+});
+
+//********************** Додаєм портфоліо через DOM **************************************
+const searchMain = document.getElementById("projects");
+
+function CreateSection(clas, text, searchBlock) {
+  const createSection = document.createElement("section");
+  createSection.setAttribute("class", `${clas}  section-card`);
+  createSection.innerHTML = text;
+  searchBlock.append(createSection);
+}
+
+function CreateArticle(obj, searchBlock) {
+  const createSection = document.createElement("article");
+  createSection.setAttribute("class", "project-card");
+  createSection.innerHTML = `
+            <div class='demoImg'>
+                <a href='${obj.url}' target='_blanck'>
+                    <img src='/img/${obj.category}/${obj.img}' alt='${obj.name}' />
+                </a>
+            </div> 
+            <h3>&nbsp;${obj.name}</h3>`;
+  searchBlock.append(createSection);
+}
+
+category.map((el, index) => {
+  CreateSection(el.urlNAme, `<h2>${el.name}</h2>`, searchMain);
+  const searchSectionCategory = document.querySelector("." + el.urlNAme);
+  CreateSection("category " + el.urlNAme + index, "", searchSectionCategory);
+  const searchSectionForArticle = document.querySelector(
+    "." + el.urlNAme + index
+  );
+
+  demos
+    .filter((obj) => {
+      return obj.category === el.urlNAme;
+    })
+    .map((obj) => CreateArticle(obj, searchSectionForArticle));
+});
